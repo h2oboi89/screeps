af929e33662c3b489ea646498090256311f4eb81
Waters <h2oboi89@gmail.com>
2019-07-19 20:27:50 -0400

    fixed typo

--- prototype.tower.js
+++ prototype.tower.js
@@ -1,7 +1,7 @@
 StructureTower.prototype.defend = function () {
     let target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-    if (taget != undefined) {
+    if (target != undefined) {
         this.attack(target);
     }
 };
